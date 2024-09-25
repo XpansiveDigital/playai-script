0dd9ce41ed04f68a6d4a5a78ef5ed97be4f9023f
XpansiveDigital <167742352+XpansiveDigital@users.noreply.github.com>
2024-09-25 16:41:21 +0800

    Delete IA TEST.js

--- IA TEST.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  if (typeof PlayAI !== 'undefined') {
-    PlayAI.open('jByeRb0kdL6O2YwCyxNzQ', {
-      container: '#playai-container'
-    });
-  } else {
-    console.error("PlayAI script failed to load.");
-  }
-});
